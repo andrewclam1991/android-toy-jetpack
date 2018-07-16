81f3dde8b10b1ed08db8811e83343a9ba3924f21
andrewclam <23492362+andrewclam1991@users.noreply.github.com>
2018-07-15 17:28:49 -0700

    add library stack to build.gradle

---
+++ app/src/main/java/com/andrewclam/toyjetpack/worker/LocationUpdateWorker.java
@@ -0,0 +1,12 @@
+package com.andrewclam.toyjetpack.worker;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+
+public class LocationUpdateWorker extends Worker {
+  @NonNull
+  @Override
+  public Worker.Result doWork() {
+    return null;
+  }
+}
